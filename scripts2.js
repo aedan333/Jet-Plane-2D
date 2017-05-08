5a2433c482f38945cbd8ffca1664be5e8f56cd66
liamlevesque <liam.levesque@feralcatlabs.com>
2017-05-08 16:58:18 -0700

    getting game-y

--- scripts2.js
+++ scripts2.js
@@ -1,18 +1,41 @@
 var canvas;
 var ctx;
 var timer;
-var posX = 0;
-var posY = 0;
-var square = new Image();
-square.src = "Chic-plane.png";
-var squareWidth = 75;
-var squareHeight = 75;
-var airport = new Image();
-airport.src = "Basic-Airport.png";
-var airportWidth = 124;
-var airportHeight = 124;
-var posX2 = 1242;
-var posY2 = 538;
+
+var squareImage = new Image();
+squareImage.src = "Chic-plane.png";
+
+var square = {
+	img: squareImage,
+	width: 75,
+	height: 75,
+	x: 0,
+	y: 0,
+}
+
+var airportImage = new Image();
+airportImage.src = "Basic-Airport.png";
+
+var airport = {
+	img: airportImage,
+	width: 124,
+	height: 124,
+	x: 1242,
+	y: 538
+};
+
+var gooseImage = new Image();
+gooseImage.src = "Goose_sm.png";
+
+function goose(){
+	this.img = gooseImage;
+	this.width = 40;
+	this.height = 29;
+	this.x = 1242;
+	this.y = 0;
+};
+
+var geese = [];
 
 
 function init(){
@@ -20,11 +43,18 @@ function init(){
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
 	
-	
-	timer=setInterval(draw, 10);
-
 	ctx.canvas.width  = window.innerWidth;
   	ctx.canvas.height = window.innerHeight;
+
+	var gooseA = new goose();
+	gooseA.y = randomYPosition();
+	geese.push(gooseA);
+
+	setInterval(makeAGoose,3000);
+	
+	timer = setInterval(draw, 10);
+
+  	//console.log(ctx.canvas.height);
 
 };
 
@@ -32,16 +62,16 @@ window.addEventListener('keydown', function(event){
 
 	switch(event.keyCode){
 		case 38:
-			posY -= 10;
+			square.y -= 10;
 			break;
 		case 40:
-			posY += 10;
+			square.y += 10;
 			break;
 		case 37:
-			posX -= 10;
+			square.x -= 10;
 			break;
 		case 39:
-			posX += 10;
+			square.x += 10;
 			break;
 	}
 
@@ -49,6 +79,50 @@ window.addEventListener('keydown', function(event){
 
 function draw(){ 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(airport, posX2, posY2, airportWidth, airportHeight); 
-	ctx.drawImage(square, posX, posY, squareWidth, squareHeight);
+	
+	for(let i = 0;i < geese.length; i++){
+		geese[i].x -=1;
+		ctx.drawImage(geese[i].img, geese[i].x, geese[i].y, geese[i].width, geese[i].height);
+		if(geese[i].x + geese[i].width < 0){
+			geese.splice(i,1);
+		}
+		if(detectCollision(square,geese[i])){
+			clearInterval(timer);
+			window.location = 'lost.html';	
+		} 
+	}
+
+	ctx.drawImage(airport.img, airport.x, airport.y, airport.width, airport.height); 
+	ctx.drawImage(square.img, square.x, square.y, square.width, square.height);
+
+	
+	//CHECK IF THE PLANE HAS TOUCHED THE AIRPORT
+	if(detectCollision(square,airport)){
+		clearInterval(timer);
+		window.location = 'winner.html';	
+	} 
 }
+
+function makeAGoose(){
+	var newGoose = new goose();
+	newGoose.y = randomYPosition();
+	geese.push(newGoose);
+}
+
+function randomYPosition(){
+	return Math.floor(Math.random() * ctx.canvas.height);
+}
+
+function detectCollision(object1, object2){
+	if (object1.x < object2.x + object2.width &&
+   object1.x + object1.width > object2.x &&
+   object1.y < object2.y + object2.height &&
+   object1.height + object1.y > object2.y) {
+	    return true;
+	}
+};
+
+
+
+
+
